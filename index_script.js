6f6a6fded731e4ba3dd1e6033588749095b21857
unknown <titanasura@gmail.com>
2023-01-26 16:55:30 -0500

    Terminada la primera funcionalidad

--- index_script.js
+++ index_script.js
@@ -1,12 +1,16 @@
 // Programa que pida dos numeros y que nos diga cual es el menor, cual es el mayor y si son iguales
 
 function mayor() {
-    var primerNum = document.getElementById("numero1").value;
-    var segundoNum = document.getElementById("numero2").value;
+    var primerNum = document.querySelector("#numero1").value;
+    var primerNum = parseInt(primerNum);
+    var segundoNum = document.querySelector("#numero2").value;
+    var segundoNum = parseInt(segundoNum);
+
+    
 
     if (primerNum > segundoNum) {
-        document.getElementById("resultado").innerHTML = "El mayor de los dos numeros es el: " + primerNum;
+        document.querySelector("#resultado").innerHTML = "El mayor de los dos numeros es el: " + primerNum;
     } else {
-        document.getElementById("resultado").innerHTML = "El mayor de los dos numeros es el: " + segundoNum;
+        document.querySelector("#resultado").innerHTML = "El mayor de los dos numeros es el: " + segundoNum;
     }
 }
